caf2ab3a681b94e5272a52f81ecd36bbdc90b0d7
Chihiro Fujita <chr0508enn1203@gmail.com>
2026-04-14 11:22:46 +0900

    List表示機能追加

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,12 +1,24 @@
 package com.example.demo;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HelloController {
 
     @GetMapping("/hello")
-    public String hello() {
-        return "Hello Spring!";
+    public String hello(Model model) {
+
+        List<String> fruits = new ArrayList<>();
+        fruits.add("apple");
+        fruits.add("orange");
+        fruits.add("banana");
+
+        model.addAttribute("fruits", fruits);
+
+        return "hello";
     }
 }
